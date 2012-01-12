26bcd0056579eb541b79a10a5026a5730df1f566
Ryan Knight <ryan@grandcloud.com>
2012-01-12 16:06:57 -0700

    updating places server

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,16 +1,61 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
+import models.DisplayPlace;
+import org.apache.commons.lang.StringUtils;
+import org.folg.places.standardize.Place;
+import org.folg.places.standardize.Standardizer;
+import org.xml.sax.SAXException;
+import play.mvc.Controller;
 
-import java.util.*;
-
-import models.*;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application extends Controller {
 
-    public static void index() {
-        render();
-    }
+   public static void index() {
+      render();
+   }
+
+   public static void searchForPlace(String place) throws IOException, SAXException {
+      List<Place> results = Standardizer.getInstance().standardize(place, 10);
+      List<DisplayPlace> displayPlaces = new ArrayList<DisplayPlace>();
+      for (Place nextPlace : results) {
+         DisplayPlace displayPlace = getDisplayPlace(nextPlace);
+         displayPlaces.add(displayPlace);
+      }
+
+      render(displayPlaces);
+   }
+
+   private static DisplayPlace getDisplayPlace(Place standardizedPlace) {
+      Standardizer standardizer = Standardizer.getInstance();
+      DisplayPlace displayPlace = new DisplayPlace();
+      displayPlace.setFullName(standardizedPlace.getFullName());
+
+      int[] alsoLocatedInIds = standardizedPlace.getAlsoLocatedInIds();
+      if ((alsoLocatedInIds != null) && (alsoLocatedInIds.length > 0)) {
+         StringBuffer alsoLocatedStrs = new StringBuffer();
+         for (int indx = 0; indx < alsoLocatedInIds.length; indx++) {
+            int alsoLocatedInId = alsoLocatedInIds[indx];
+            Place alsoLocatedPlace = standardizer.lookupPlace(alsoLocatedInId);
+            if (alsoLocatedStrs.length() > 0) {
+               alsoLocatedStrs.append(", ");
+            }
+            alsoLocatedStrs.append(alsoLocatedPlace.getFullName());
+         }
+         displayPlace.setAlsoLocatedIn(alsoLocatedStrs.toString());
+      }
+
+      String altNames = StringUtils.join(standardizedPlace.getAltNames(), ",");
+      displayPlace.setAltNames(altNames);
+
+      String types = StringUtils.join(standardizedPlace.getTypes(), ",");
+      displayPlace.setTypes(types);
+
+      displayPlace.setLatitude(standardizedPlace.getLatitude());
+      displayPlace.setLongitude(standardizedPlace.getLongitude());
+      return displayPlace;
+   }
 
 }
--- app/controllers/Bootstrap.java
+++ app/controllers/Bootstrap.java
@@ -1,8 +1,19 @@
 package controllers;
 
+import org.folg.places.standardize.Standardizer;
+import play.jobs.Job;
+import play.jobs.OnApplicationStart;
+
 /**
- * User: ryan
+ * User: Ryan K
  * Date: 1/12/12
  */
-public class Bootstrap {
+@OnApplicationStart
+public class Bootstrap extends Job {
+
+   public void doJob() {
+      //get the standardizer to force initialization
+      Standardizer standardizer = Standardizer.getInstance();
+      System.out.println("standardizer = " + standardizer);
+   }
 }
